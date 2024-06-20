e840f5fc5ea7392934a451fb7fd537463fa41e0b
Sariahmdsad <167486662+Sariahmdsad@users.noreply.github.com>
2024-06-20 14:05:16 +0300

    Add files via upload

---
+++ xxx.js
@@ -0,0 +1,72 @@
+let x=document.getElementById("gh")
+let xx=document.getElementById("cv")
+let xxx=document.getElementById("vc")
+let h=document.getElementById("h")
+let fd=document.getElementById("fd")
+
+function i() {
+let w=Math.floor(Math.random() *18)+1;
+  fd.play()
+let q=document.getElementById("q").value
+  x.innerText =q;
+let cc=Math.floor(Math.random()*10)+1;
+  console.log(cc)
+if (cc==1) {
+  xx.innerText="شرب القهوه بكثرة";
+} else if (cc==2) {
+  xx.innerText="سرقت ساعة";
+} else if (cc==3) {
+  xx.innerText="الوزن الزائد";
+} else if (cc==4) {
+  xx.innerText="قتل عشر اشخص";
+} else if (cc==5) {
+  xx.innerText="العب وقت طويل";
+} else if (cc==6) {
+  xx.innerText="ترك قريه دون حرقه";
+} else if (cc==7) {
+  xx.innerText="استعمل شدقن حرق";
+} else if (cc==8) {
+  xx.innerText="استعمل شدقن حرق ذهبي ";
+} else if (cc==9) {
+  xx.innerText="اسرعه الجنونيه";
+} else if (cc==10) {
+  xx.innerText="ميصلي الجمعه";
+}
+if (w==1) {
+  xxx.innerText="اعدم برصص";
+} else if (w==2) {
+  xxx.innerText="اعدم شنقن";
+} else if (w==3) {
+  xxx.innerText="ارجم عتا الموت";
+} else if (w==4) {
+  xxx.innerText="القرفة البيضأ";
+} else if (w==5) {
+  xxx.innerText="قصص";
+} else if (w==6) {
+  xxx.innerText="التبوت";
+} else if (w==7) {
+  xxx.innerText="ارمي فل حمم";
+} else if (w==8) {
+  xxx.innerText="اضرب حتا الموت";
+} else if (w==9) {
+  
+} else if (w==10) {
+  xxx.innerText="الحصان الحديدي";
+}  else if (w==11) {
+  xxx.innerText="ارمي فل بحر";
+} else if (w==12) {
+  xxx.innerText="اعدم ب RGB";
+} else if (w==13) {
+  xxx.innerText="تسمع دوره ٣ ايام";
+} else if (w==14) {
+  xx.innerText="اعدم ب دبابه";
+} else if (w==15) {
+  xx.innerText="اعدم بصبر";
+} else if (w==16) {
+  xx.innerText="بكس من ماش";
+} else if (w==17) {
+  xx.innerText="كف من قوكو";
+} else if (w==18) {
+  xx.innerText="غصب زينو";
+}
+}
